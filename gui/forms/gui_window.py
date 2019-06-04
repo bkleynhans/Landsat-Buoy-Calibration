aca01caa3210eca67d511c915223d014e35a88ec
Benjamin Kleynhans <kleynhansbenjamin@gmail.com>
2019-06-04 11:58:38 -0400

    Add parent class for windows (TopLevels) within the gui

---
+++ gui/forms/gui_window.py
@@ -0,0 +1,44 @@
+###
+#
+# CIS Top of Atmosphere Radiance Calibration
+#
+# Program Description : GUI for the Landsat Buoy Calibration program
+# Created By          : Benjamin Kleynhans
+# Creation Date       : May 30, 2019
+# Authors             : Benjamin Kleynhans
+#
+# Last Modified By    : Benjamin Kleynhans
+# Last Modified Date  : May 30, 2019
+# Filename            : gui_window.py
+#
+###
+
+# Imports
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+from gui.forms import settings_notebook
+
+
+class Gui_Window():
+    
+    # Create frame container
+    frames = {}
+        
+    # Settings Window constructor
+    def __init__(self, master, window_name, window_title):
+                
+        self.window_name = window_name
+        self.window_title = window_title
+        
+        self.create_gui_window(master)
+        
+    
+    # Create the actual window as a separate window
+    def create_gui_window(self, master):
+        
+        self.gui_window = Toplevel(master)
+        
+        self.gui_window.frames = Gui_Window.frames
+        
+        master.windows[self.window_name] = self.gui_window

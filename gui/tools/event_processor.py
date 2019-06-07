5f4166695da67d2c98156eac781a79a38e48dd8e
Benjamin Kleynhans <kleynhansbenjamin@gmail.com>
2019-06-07 14:31:42 -0400

    Created a watcher program to check for file changes

---
+++ gui/tools/event_processor.py
@@ -0,0 +1,42 @@
+###
+#
+# CIS Top of Atmosphere Radiance Calibration
+#
+# Program Description : GUI for the Landsat Buoy Calibration program
+# Created By          : Benjamin Kleynhans
+# Creation Date       : June 7, 2019
+# Authors             : Benjamin Kleynhans
+#
+# Last Modified By    : Benjamin Kleynhans
+# Last Modified Date  : June 7, 2019
+# Filename            : event_processor.py
+#
+###
+
+# Imports
+import pyinotify
+
+
+class Event_Processor(pyinotify.ProcessEvent):
+    
+    _methods = ["IN_CREATE",
+                "IN_OPEN",
+                "IN_ACCESS",
+                "IN_ATTRIB",
+                "IN_CLOSE_NOWRITE",
+                "IN_CLOSE_WRITE",
+                "IN_DELETE",
+                "IN_DELETE_SELF",
+                "IN_IGNORED",
+                "IN_MODIFY",
+                "IN_MOVE_SELF",
+                "IN_MOVED_FROM",
+                "IN_MOVED_TO",
+                "IN_Q_OVERFLOW",
+                "IN_UNMOUNT",
+                "default"]
+    
+    # Event Processor constructor
+    def __init__(self):
+        
+        pass
---
+++ gui/tools/file_watcher.py
@@ -0,0 +1,48 @@
+###
+#
+# CIS Top of Atmosphere Radiance Calibration
+#
+# Program Description : GUI for the Landsat Buoy Calibration program
+# Created By          : Benjamin Kleynhans
+# Creation Date       : June 7, 2019
+# Authors             : Benjamin Kleynhans
+#
+# Last Modified By    : Benjamin Kleynhans
+# Last Modified Date  : June 7, 2019
+# Filename            : file_watcher.py
+#
+###
+
+# Imports
+import os
+import pyinotify
+from event_processor import Event_Processor
+
+
+class File_Watcher():
+    
+    # File Watcher constructor
+    def __init__(self, path):
+        
+        self.event_processor = Event_Processor()
+        
+        for method in Event_Processor._methods:
+            self.process_generator(Event_Processor, method)
+        
+        self.watch_manager = pyinotify.WatchManager()
+        self.event_notifier = pyinotify.ThreadedNotifier(self.watch_manager, Event_Processor())
+        
+        self.watch_this = path
+        self.watch_manager.add_watch(self.watch_this, pyinotify.IN_MODIFY)
+        self.event_notifier.start()
+        
+    
+    def process_generator(self, cls, method):
+        def _method_name(self, event):
+            print("Method name: process_{}()\n"
+                   "Path name: {}\n"
+                   "Event Name: {}\n".format(method, event.pathname, event.maskname))
+            
+            
+        _method_name.__name__ = "process_{}".format(method)
+        setattr(cls, _method_name.__name__, _method_name)

8020720f4bbd43a7adbccea8b6a331f973f6795d
Benjamin Kleynhans <kleynhansbenjamin@gmail.com>
2019-06-13 10:47:06 -0400

    Add functionality to have batch be processed from both the command line window as well as the gui.  This includes temporary log file generation for output display to gui as well as output for reporting and graphs.

--- tools/forward_model_batch.py
+++ tools/forward_model_batch.py
@@ -26,8 +26,9 @@ import cv2
 import db_operations
 import settings
 
+
 # Process the SceneIDs and print results to a file
-def batch_f_model(scene_txt, scenes, output_txt, display_image, db_operator=None):
+def batch_f_model(args, scene_txt, scenes, output_txt, display_image, caller, status_logger, output_logger, db_operator=None):
 
     output_txt += scene_txt[scene_txt.rfind('/') + 1:scene_txt.rfind('.')] + '.out'
     
@@ -39,11 +40,14 @@ def batch_f_model(scene_txt, scenes, output_txt, display_image, db_operator=None
 
     with open(output_txt, 'w') as f:
         
+        report_headers = ('Scene_ID, Date, Buoy_ID, bulk_temp, skin_temp, buoy_lat, buoy_lon, mod1, mod2, img1, img2, error1, error2, status, reason')
+        
+        print(report_headers, file = f)
+        
+        print_data(caller, report_headers, output_logger)
+        
         graph_data_band10 = np.zeros([1,3])
         graph_data_band11 = np.zeros([1,3])
-                
-        f.write('Scene_ID, Date, Buoy_ID, bulk_temp, skin_temp, buoy_lat, buoy_lon, mod1, mod2, img1, img2, error1, error2, status, reason\n')
-        f.flush()
         
         # For each SceneID, process and write to txt file
         for i, scene_id in enumerate(scenes):
@@ -53,14 +57,16 @@ def batch_f_model(scene_txt, scenes, output_txt, display_image, db_operator=None
                 scene_id = scene_id[:(scene_id.index(' '))]
             elif scene_id[-1] == '\n':
                 scene_id = scene_id[:-1]
-            
-            print("\n Starting analysis of scene {}".format(scene_id))
 
-            ret = forward_model.main([scene_id, '-c forward_model_batch', '-n' + display_image])
-
-            sys.stdout.write("\r Analysis completed for scene {}\n\n".format(scene_id))
+            log_text = "\n Starting analysis of scene {}\n".format(scene_id)
             
-            print(scene_id.replace('\n', ','),ret)
+            print_data(caller, log_text, status_logger)
+            
+            ret = forward_model.main([scene_id, ('-c' + caller), ('-n' + display_image),('-t' + args.statuslog), ('-u' + args.outputlog)])
+
+            log_text = "\n Analysis completed for scene {}\n".format(scene_id)
+            
+            print_data(caller, log_text, status_logger)
 
             for key in ret.keys():
                 if(ret[key][9] == "failed"):
@@ -87,16 +93,21 @@ def batch_f_model(scene_txt, scenes, output_txt, display_image, db_operator=None
                         graph_data_band10 = np.vstack((graph_data_band10, [date, difference, error[band]]))
                     elif band == 11:
                         graph_data_band11 = np.vstack((graph_data_band11, [date, difference, error[band]]))
-                        
-                print(scene_id.replace('\n', ''), date.strftime('%Y/%m/%d'), buoy_id, bulk_temp, skin_temp, buoy_lat, buoy_lon, \
-                    *mod_ltoa.values(), *img_ltoa.values(), *error.values(), status, error_message, file=f, sep=', ')
-#                else:
-#                    if ((ret[key][10] == "data") or
-#                        (ret[key][10] == "merra_layer1_temperature")):
-#                        print(scene_id.replace('\n', ''), error_message, ret[key][0], file=f, sep=', ')
-#                    else:
-#                        print(scene_id.replace('\n', ''), error_message, file=f, sep=', ')
-
+                
+#                print(scene_id.replace('\n', ''), date.strftime('%Y/%m/%d'), buoy_id, bulk_temp, skin_temp, buoy_lat, buoy_lon, \
+#                    *mod_ltoa.values(), *img_ltoa.values(), *error.values(), status, error_message, file=f, sep=', ')
+                        # Convert tuple to text and remove first and last parentheses
+                
+                # Create a formatted tuple with the required data
+                processed_data = (scene_id.replace('\n', ''), date.strftime('%Y/%m/%d'), buoy_id, bulk_temp, skin_temp, buoy_lat, buoy_lon, *mod_ltoa.values(), *img_ltoa.values(), *error.values(), status, error_message)
+                # Convert tuple to text and remove first and last parentheses
+                processed_data = "".join(str(processed_data))[1:-1]
+                
+                # Save the processed data to the output file
+                print(processed_data, file = f)
+                
+                print_data(caller, processed_data, output_logger)
+                
             f.flush()
     
     graph_data_band10 = np.delete(graph_data_band10, (0), axis=0)
@@ -123,19 +134,51 @@ def batch_f_model(scene_txt, scenes, output_txt, display_image, db_operator=None
             output_file_no_band + "_band11")                                    # Output file name (no extension)
 
 
+
+def save_to_file(caller, processed_data):
+    
+    
+    pass
+
+
+def print_data(caller, data, logger):
+    
+    if caller == 'forward_model_batch':
+        
+        print_to_screen(data)
+        
+    elif caller == 'tarca_gui_batch':
+                
+        logger.write(data)
+
+
+def print_to_screen(data):
+
+    sys.stdout.write("\n" + data)
+
+
 # Read the supplied batch file
 def buildModel(args):
     
-    #if settings.USE_MYSQL:
-    #    db_operator = db_operations.Db_Operations()
-
-    scenes = open(args.scene_txt).readlines()
+    from process_logger import Process_Logger    
     
+    status_logger = Process_Logger(args.statuslog)
+    output_logger = Process_Logger(args.outputlog)
+    
+    if (args.caller == 'menu'):
+        args.caller = 'forward_model_batch'
+            
     if settings.USE_MYSQL:
         db_operator = db_operations.Db_Operations()
-        batch_f_model(args.scene_txt, scenes, args.save, args.display_image, db_operator)
+
+    scenes = open(args.scene_txt).readlines()
+        
+    if settings.USE_MYSQL:
+        db_operator = db_operations.Db_Operations()
+        batch_f_model(args, args.scene_txt, scenes, args.save, args.display_image, db_operator, args.caller, status_logger, output_logger)
     else:
-        batch_f_model(args.scene_txt, scenes, args.save, args.display_image)
+        batch_f_model(args, args.scene_txt, scenes, args.save, args.display_image, args.caller, status_logger, output_logger)
+
 
 
 # Parse the arguments received during program launch
@@ -150,6 +193,11 @@ def parseArgs(args):
     parser.add_argument('-s', '--save', default='output/batches/data/')
     # Allow ability to disable image display
     parser.add_argument('-n', '--display_image', default='true')
+# Add caller information
+    parser.add_argument('-c', '--caller', help='The name or reference of the file calling this function')
+# Add log file locations
+    parser.add_argument('-t', '--statuslog', default = 'logs/status/default.status', help='Status file directory')
+    parser.add_argument('-u', '--outputlog', default = 'logs/output/default.output', help='Output file directory')
 
     return parser.parse_args(args)
 

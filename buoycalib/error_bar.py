b78cf004c469e8e2c01c262fe724aa51139cb381
Benjamin Kleynhans <kleynhansbenjamin@gmail.com>
2019-06-25 12:41:46 -0400

    Change file to use new modtran_data dictionary of values instead of previous individual variables

--- buoycalib/error_bar.py
+++ buoycalib/error_bar.py
@@ -1,4 +1,6 @@
 import numpy
+import pdb
+import datetime, sys
 
 from . import (modtran, atmo, sat, radiance, settings)
 from .atmo import merra
@@ -12,8 +14,10 @@ def error_bar(scene_id, buoy_id, skin_temp, skin_temp_std, overpass_date, buoy_l
         for i, atmo in enumerate(atmos):
 
             modtran_directory = '{0}/{1}_{2}_{3}_{4}'.format(settings.MODTRAN_BASH_DIR, scene_id, buoy_id, temp, i)
-            wavelengths, upwell_rad, gnd_reflect, transmission = modtran.process(atmo, buoy_lat, buoy_lon, overpass_date, modtran_directory, temp)
-            mod_ltoa_spectral = radiance.calc_ltoa_spectral(wavelengths, upwell_rad, gnd_reflect, transmission, temp)
+            
+#            wavelengths, upwell_rad, gnd_reflect, transmission = modtran.process(atmo, buoy_lat, buoy_lon, overpass_date, modtran_directory, temp)
+            modtran_data = modtran.process(atmo, buoy_lat, buoy_lon, overpass_date, modtran_directory, temp)
+            mod_ltoa_spectral = radiance.calc_ltoa_spectral(modtran_data['wavelengths'], modtran_data['upwell_rad'], modtran_data['gnd_reflect'], modtran_data['transmission'], temp)
 
             for b in bands:
                 if scene_id[0:3] == 'MOD':
@@ -21,7 +25,7 @@ def error_bar(scene_id, buoy_id, skin_temp, skin_temp_std, overpass_date, buoy_l
                 elif scene_id[0:3] in ('LC8', 'LC0'):
                     RSR_wavelengths, RSR = numpy.loadtxt(rsrs[b], unpack=True)
 
-                modeled_ltoas[b].append(radiance.calc_ltoa(wavelengths, mod_ltoa_spectral, RSR_wavelengths, RSR))
+                modeled_ltoas[b].append(radiance.calc_ltoa(modtran_data['wavelengths'], mod_ltoa_spectral, RSR_wavelengths, RSR))
 
     error = {b:numpy.asarray(modeled_ltoas[b]).std() for b in modeled_ltoas}
     return error

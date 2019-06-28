5a24cf8870830f39b8ecd35c5ff7be16b952cf6c
Benjamin Kleynhans <kleynhansbenjamin@gmail.com>
2019-06-28 14:32:13 -0400

    Update logger calls to allow for different loggers based on passed parameters.  This allows logs to be directed to different files and windows

--- modules/core/landsat/landsat_batch.py
+++ modules/core/landsat/landsat_batch.py
@@ -16,11 +16,7 @@
 
 # Imports
 import sys, pdb
-from buoycalib import (sat, buoy, atmo, radiance, modtran, settings, download, display, error_bar)
 from modules.core.landsat.landsat_single import Landsat_Single
-from modules.core import model
-#from tools import test_paths
-import datetime
 
 
 class Landsat_Batch(Landsat_Single):
@@ -29,31 +25,41 @@ class Landsat_Batch(Landsat_Single):
         
         super(Landsat_Single, self).__init__(args)
         
+        self.build_file_paths()
+        
+        heading_counter = 0
+        
         for scene in self.args['landsat_scenes']:
             sys.stdout.write('\n')
-            self.log("  Starting analysis of scene {}".format(scene))
+            self.log('status', "  Starting analysis of scene {}".format(scene))
             sys.stdout.write('\n')
             
             self.download_image(scene)
             self.analyze_image()
             
             sys.stdout.write('\n')
-            self.log("  Analysis completed for scene {}".format(scene))
+            self.log('status', "  Analysis completed for scene {}".format(scene))
             sys.stdout.write('\n')
             sys.stdout.flush()
+            
+            if self.args['caller'] == 'tarca_gui':
+                if heading_counter == 0:
+                    heading_counter += 1
+                    self.print_report_headings()
+            else:
+                self.print_report_headings()
             
             self.process_scene()
             self.finalize()
         
         
-    def process_scene(self):
+    def build_file_paths(self):
         
         # Build the output file path from the input file path
         batchfile_name = self.args['batch_file_name'][(self.args['batch_file_name'].rfind('/') + 1):]        
         self.args['savefile'] = self.args['savefile'][:self.args['savefile'].rfind('/') + 1] + batchfile_name[:batchfile_name.rfind('.')] + '.out'
         
-        report_headings = "Scene_ID, Date, Buoy_ID, bulk_temp, skin_temp, buoy_lat, buoy_lon, mod1, mod2, img1, img2, error1, error2, status, reason"
         
-        self.log(report_headings)
+    def process_scene(self):
         
-        self.print_output_to_screen()
+        self.print_and_save_output()
